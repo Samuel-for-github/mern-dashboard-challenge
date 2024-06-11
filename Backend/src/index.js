a5be2322ec454b42e08e2d17c564268e009ca938
Samuel-for-github <134806257+Samuel-for-github@users.noreply.github.com>
2024-06-12 03:16:54 +0530

    project setup - part 2

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,19 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
+import {app} from "./app.js";
 
-
-connectDB()
+connectDB().then(()=>{
+    app.on("error", (error)=>{
+        console.log("Error while starting app: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT||3000,()=>{
+        console.log("Server started on port: " + process.env.PORT || 3000);
+    })
+}).catch((error)=>{
+    console.log("MongoDB connection Failed:", error);
+})
 
 
 
